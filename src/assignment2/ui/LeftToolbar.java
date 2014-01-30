d108c167d8f1c41a40b2ca321efe994daa7d21c7
David Eriksson <david@UL30VT-13.10>
2014-01-30 08:56:16 +0100

    Renamed class

--- src/assignment2/ui/LeftToolbar.java
+++ src/assignment2/ui/LeftToolbar.java
@@ -15,37 +15,36 @@ import javax.swing.border.EmptyBorder;
 
 import assignment2.globals.Constants;
 import assignment2.globals.TextStyleDocument;
+import assignment2.ui.panels.CoordinatesPanel;
 import assignment2.ui.panels.PolygonPickerPanel;
 import assignment2.ui.panels.PyramidSpecificationPanel;
 import assignment2.ui.panels.SquareSpecificationPanel;
 import assignment2.ui.panels.StarSpecificationPanel;
+import assignment2.ui.uiParts.LableText;
 
 @SuppressWarnings("serial")
-public class CreatePolygonToolbar extends Panel implements ActionListener {
+public class LeftToolbar extends Panel implements ActionListener {
 	
 	private Color backgroundColor = Constants.backgroundColor;
 	
-	private PolygonPickerPanel polygonPickerPanel; //Consists of a single JCombobox
-	private JPanel coordinatesPanel; //JTextField for coordinates x, y and z are at index 1, 3 and 5 respectively
-	private JButton createPolygonButton;
+	private PolygonPickerPanel polygonPickerPanel = new PolygonPickerPanel();
+	private CoordinatesPanel coordinatesPanel = new CoordinatesPanel();
 
 	private PyramidSpecificationPanel pyramidSpecPanel = new PyramidSpecificationPanel();
 	private SquareSpecificationPanel squareSpecPanel = new SquareSpecificationPanel();
 	private StarSpecificationPanel starSpecPanel = new StarSpecificationPanel();
 	
-	private TextStyleDocument textStyle = new TextStyleDocument();
+	private JButton createPolygonButton;
 	
 	private final static int BOTTOM_MARGIN_SMALL = 10;
 	
-	public CreatePolygonToolbar() {
+	public LeftToolbar() {
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-//		this.setLayout(new CardLayout());
 		this.setBackground(backgroundColor);
 		
 		polygonPickerPanel = new PolygonPickerPanel();
 		polygonPickerPanel.addActionListener(this);
 		polygonPickerPanel.setBorder(new EmptyBorder(0, 0, BOTTOM_MARGIN_SMALL, 0));
-		coordinatesPanel = getCoordinatesPanel(this.backgroundColor, 2);
 		coordinatesPanel.setBorder(new EmptyBorder(0, 0, BOTTOM_MARGIN_SMALL, 0));
 		
 		createPolygonButton = new JButton("Create");
@@ -59,51 +58,24 @@ public class CreatePolygonToolbar extends Panel implements ActionListener {
 		this.add(buttonPanel);
 	}
 	
-	private JPanel getCoordinatesPanel(Color backgrouColor, int numberColumns)	{
-		LableText xInputLable = new LableText("X:");
-		JTextField xInput = new JTextField("", numberColumns);
-
-		LableText yInputLable = new LableText("Y:");
-		JTextField yInput = new JTextField("", numberColumns);
-		
-		LableText zInputLable = new LableText("Z:");
-		JTextField zInput = new JTextField("", numberColumns);
-		
-		JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		panel.setBackground(backgroundColor);
-		panel.add(xInputLable);
-		panel.add(xInput);
-		panel.add(yInputLable);
-		panel.add(yInput);
-		panel.add(zInputLable);
-		panel.add(zInput);
-		return panel;
+	public void addButtonActionListener(ActionListener actionLitstener)	{
+		createPolygonButton.addActionListener(actionLitstener);
 	}
 	
-	private JPanel getPyramidSpecPanel(Color backgroundColor)	{
-		LableText baseWidthLable = new LableText("Base Width:");
-		JTextField baseWidthInput = new JTextField("", 2);
-		
-		JPanel baseWidthPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		baseWidthPanel.setBackground(backgroundColor);
-		baseWidthPanel.add(baseWidthLable);
-		baseWidthPanel.add(baseWidthInput);
-		
-		LableText heightLable = new LableText("Height:");
-		heightLable.setBorder(new EmptyBorder(0, 0, 0, 36)); //Makes both the boxes leveled
-		JTextField heightInput = new JTextField("", 2);
-		
-		JPanel heightPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		heightPanel.setBackground(backgroundColor);
-		heightPanel.add(heightLable);
-		heightPanel.add(heightInput);
-		
-		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		panel.setBackground(backgroundColor);
-		panel.add(baseWidthPanel);
-		panel.add(heightPanel);
-		return panel;
+	public String getActivePolygon()	{
+		return polygonPickerPanel.getCurrentPolygon();
+	}
+	
+	public float getXCoordinate() throws NumberFormatException	{
+		return coordinatesPanel.getXValue();
+	}
+	
+	public float getYCoordinate() throws NumberFormatException	{
+		return coordinatesPanel.getYValue();
+	}
+	
+	public float getZCoordinate() throws NumberFormatException	{
+		return coordinatesPanel.getZValue();
 	}
 	
 	/**
