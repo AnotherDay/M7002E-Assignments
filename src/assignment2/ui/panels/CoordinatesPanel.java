075a17266d68f0a2905c9da2e0825f0034e4dfab
David Eriksson <david@UL30VT-13.10>
2014-01-30 08:54:19 +0100

    Moved coordinates panel to separate class

---
+++ src/assignment2/ui/panels/CoordinatesPanel.java
@@ -0,0 +1,47 @@
+package assignment2.ui.panels;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import assignment2.globals.Constants;
+import assignment2.ui.uiParts.LableText;
+
+public class CoordinatesPanel extends JPanel {
+	
+	private final static int numberOfColumns = 2;
+	private JTextField xInput, yInput, zInput;
+	
+	public CoordinatesPanel()	{
+		LableText xInputLable = new LableText("X:");
+		xInput = new JTextField("", numberOfColumns);
+
+		LableText yInputLable = new LableText("Y:");
+		yInput = new JTextField("", numberOfColumns);
+		
+		LableText zInputLable = new LableText("Z:");
+		zInput = new JTextField("", numberOfColumns);
+		
+		this.setLayout(new FlowLayout(FlowLayout.LEFT));
+		this.setBackground(Constants.backgroundColor);
+		this.add(xInputLable);
+		this.add(xInput);
+		this.add(yInputLable);
+		this.add(yInput);
+		this.add(zInputLable);
+		this.add(zInput);
+	}
+	
+	public float getXValue() throws NumberFormatException	{
+		return Float.valueOf(xInput.getText());
+	}
+	
+	public float getYValue() throws NumberFormatException	{
+		return Float.valueOf(yInput.getText());
+	}
+	
+	public float getZValue() throws NumberFormatException	{
+		return Float.valueOf(zInput.getText());
+	}
+}
