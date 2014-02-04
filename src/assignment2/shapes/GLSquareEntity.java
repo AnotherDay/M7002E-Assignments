50fdccd728750529d26452214a020ca5c7efa140
David Eriksson <david@UL30VT-13.10>
2014-02-04 13:52:32 +0100

    Fixed square reshape bug

--- src/assignment2/shapes/GLSquareEntity.java
+++ src/assignment2/shapes/GLSquareEntity.java
@@ -19,18 +19,18 @@ public class GLSquareEntity extends GLEntity {
 	public void draw(GL2 gl, GLU glu) {
 		if(edgeLength == 0) throw new IllegalArgumentException("Radius cannot be zero");
 		else	{
-			edgeLength = edgeLength/2;
+			float halfEdgeLength = edgeLength/2;
 			gl.glPushMatrix();
 			gl.glPushAttrib(GL2.GL_ALL_ATTRIB_BITS);
-			gl.glTranslatef(xPos, yPos, zPos);
-			gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
-			gl.glColor3d(0, 0, 255);
-			gl.glBegin(GL2.GL_QUADS);
-			gl.glVertex3f(-edgeLength, -edgeLength, 0); //Bottom left vertex
-			gl.glVertex3f(edgeLength, -edgeLength, 0); //Bottom right vertex
-			gl.glVertex3f(edgeLength, edgeLength, 0); //Top right vertex
-			gl.glVertex3f(-edgeLength, edgeLength, 0); //Top left vertex
-			gl.glEnd();
+				gl.glTranslatef(xPos, yPos, zPos);
+				gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+				gl.glColor3d(0, 0, 255);
+				gl.glBegin(GL2.GL_QUADS);
+					gl.glVertex3f(-halfEdgeLength, -halfEdgeLength, 0); //Bottom left vertex
+					gl.glVertex3f(halfEdgeLength, -halfEdgeLength, 0); //Bottom right vertex
+					gl.glVertex3f(halfEdgeLength, halfEdgeLength, 0); //Top right vertex
+					gl.glVertex3f(-halfEdgeLength, halfEdgeLength, 0); //Top left vertex
+				gl.glEnd();
 			gl.glPopAttrib();
 			gl.glPopMatrix();
 		}
